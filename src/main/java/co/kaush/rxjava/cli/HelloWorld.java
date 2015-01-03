375fd9fb10b6bc1cf4a67ed612bcfd6dd93a7f79
Kaushik Gopal <code@kaush.co>
2015-01-03 12:00:58 -0800

    fix: use buffer example from here: https://github.com/ReactiveX/RxJava/issues/1176

--- src/main/java/co/kaush/rxjava/cli/HelloWorld.java
+++ src/main/java/co/kaush/rxjava/cli/HelloWorld.java
@@ -11,17 +11,12 @@ public class HelloWorld {
     Observable<Integer> source1 = Observable.just(1, 2, 3, 4, 5);
     Observable<Integer> source2 = Observable.range(100, 5);
 
-    source1.take(3)
-        .asObservable()
-        .doOnSubscribe(() -> System.out.println(" ---- subscribing"))
-        .subscribe(integer -> System.out.println(String.format("---- %d", integer)));
-
-    /*source1.take(20)
+    source1.take(20)
         .buffer(2, 1)
         .filter(l -> l.size() == 2)
         .map(l -> l.get(0) + l.get(1))
         .concatWith(source2)
-        .subscribe(integer -> System.out.println(String.format("---- %d", integer)));*/
+        .subscribe(integer -> System.out.println(String.format("---- %d", integer)));
   }
 
 }
